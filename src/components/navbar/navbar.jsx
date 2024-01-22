7d46585eb5f09bb67cdd0272de295e774b79515d
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-01-23 00:10:25 +1100

    added top nav as well as the side nav drawer with the effect

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   RiMenuLine,
   RiSearchLine,
   RiShoppingCart2Fill,
+  RiCloseLine,
+  RiTruckLine,
+  RiHeartFill,
+  RiWalletFill,
+  RiQuestionFill,
+  RiArrowDownCircleFill,
+  RiGroupFill,
+  RiPriceTag3Fill,
 } from "@remixicon/react";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => {
+  const [sideNavIsOpen, setSideNavIsOpen] = useState(false);
+
+  const handleSideNavClick = () => {
+    setSideNavIsOpen(!sideNavIsOpen);
+    console.log("okay");
+  };
+
   return (
     <div className={styles.container}>
       {/* Left side*/}
       <div className={styles.leftNavbar}>
-        <RiMenuLine size={36} className={styles.menubarIcon} />
+        <RiMenuLine
+          size={36}
+          className={styles.menubarIcon}
+          onClick={handleSideNavClick}
+        />
 
         <h1 className={styles.titleText}>
-          Best <span className={styles.textEats}>Eats</span>
+          Taste <span className={styles.textEats}>Hub</span>
         </h1>
         <div className={styles.deliveryOptions}>
           <p className={styles.option1}>Delivery</p>
@@ -34,7 +53,63 @@ const Navbar = () => {
       </button>
 
       {/* Mobile Menu */}
-      <div className={styles.mobileView}></div>
+      {sideNavIsOpen && (
+        <>
+          {/* Overlay */}
+          <div className={`${styles.mobileView} `} />
+
+          {/* Side Drawer */}
+          <div
+            className={`${styles.sideDrawer} ${
+              sideNavIsOpen ? styles.open : ""
+            }`}
+          >
+            <RiCloseLine
+              size={30}
+              className={styles.closeIcon}
+              onClick={handleSideNavClick}
+            />
+            <h2>
+              Taste <span>Hub</span>
+            </h2>
+            <nav className={styles.sideNavigation}>
+              <ul>
+                <li>
+                  <RiTruckLine color="#cb4154" className={styles.icon} />
+                  Orders
+                </li>
+                <li>
+                  <RiHeartFill color="#cb4154" className={styles.icon} />
+                  Favorites
+                </li>
+                <li>
+                  <RiWalletFill color="#cb4154" className={styles.icon} />
+                  Wallet
+                </li>
+                <li>
+                  <RiQuestionFill color="#cb4154" className={styles.icon} />
+                  Help
+                </li>
+                <li>
+                  <RiPriceTag3Fill color="#cb4154" className={styles.icon} />
+                  Promotions
+                </li>
+                <li>
+                  <RiArrowDownCircleFill
+                    color="#cb4154"
+                    className={styles.icon}
+                  />
+                  Best One
+                </li>
+                <li>
+                  <RiGroupFill color="#cb4154" className={styles.icon} />
+                  Invite Friends
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </>
+      )}
     </div>
   );
 };
